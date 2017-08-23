f7e1c87df4fbad7fbe325e49673ef5cc618a8939
Marcin Nowak <Marcin.Nowak@sonyx.net>
2017-08-23 22:30:28 +0200

    rotation encoder in

--- mosquitto-client/mosquitto-client.ino
+++ mosquitto-client/mosquitto-client.ino
@@ -5,24 +5,33 @@
 WiFiClient net;
 MQTTClient client;
 
-String message("world2");
+int pinA = 18; // Connected to CLK on KY­040
+int pinB = 5; // Connected to DT on KY­040
+int encoderPosCount = 0;
+int pinALast;
+int aVal;
+int edgeSensor;
+int lastEdgeSensor;
+boolean bCW;
+
+
+String message("world3");
 
 unsigned long lastMillis = 0;
 
 void setup() {
   WiFi.begin(ssid, pass);
 
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
+  // You need to set the IP address directly.
+  client.begin("10.10.1.3", net);
+  client.onMessage(messageReceived);
     pinMode(23, OUTPUT);      // set the LED pin mode
     pinMode(22, OUTPUT);      // set the LED pin mode
     pinMode(21, OUTPUT);      // set the LED pin mode
     pinMode(19, OUTPUT);      // set the LED pin mode
 
-  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
-  // You need to set the IP address directly.
-  client.begin("10.10.1.3", net);
-  client.onMessage(messageReceived);
-
-  connect();
+  connect();  
 }
 
 void connect() {
@@ -30,7 +39,7 @@ void connect() {
     delay(1000);
   }
 
-  while (!client.connect("arduino", "try", "try")) {
+  while (!client.connect("rotation")) {
     delay(1000);
   }
 
@@ -55,11 +64,43 @@ void loop() {
     lastMillis = millis();
     client.publish("/hello", message);
   }
+
+  aVal = digitalRead(pinA);
+
+  if (aVal != pinALast){ // Means the knob is rotating
+    // if the knob is rotating, we need to determine direction
+    // We do that by reading pin B.
+    if (digitalRead(pinB) != aVal) { // Means pin A Changed first ­ We're Rotating Clockwise
+      encoderPosCount ++;
+      bCW = true;
+    } else {// Otherwise B changed first and we're moving CCW
+      bCW = false;
+      encoderPosCount--;
+      }
+    if (bCW){
+        client.publish("/rotated", "clockwise");
+    }else{
+        client.publish("/rotated", "counterclockwise");
+    }
+    client.publish("/position", String(encoderPosCount));
+  }
+  pinALast = aVal;
+
+
+  edgeSensor = digitalRead(17);
+  if( edgeSensor != lastEdgeSensor ){
+    lastEdgeSensor = edgeSensor;
+    if( edgeSensor == 0 ) {
+      client.publish("/position", "open");    
+    }
+    else{
+      client.publish("/position", "closed");          
+    }
+  }
 }
 
 void messageReceived(String &topic, String &payload) {
-  //Serial.println("incoming: " + topic + " - " + payload);
-  char topicChar[1024];
+    char topicChar[1024];
   topic.toCharArray(topicChar,256);
   int payloadInt = payload.toInt();
   String son = String("/on");
@@ -99,9 +140,7 @@ void messageReceived(String &topic, String &payload) {
       case 4: 
         digitalWrite(19, LOW); 
         break;      
-    }
-    
+    }    
   }
-//  sleep(1000);
-//  digitalWrite(23, LOW); 
+
 }
